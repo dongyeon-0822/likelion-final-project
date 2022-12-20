1a11260f780c0ec3bbd6daf2d9e28783415ac9d9
DongYeon Kang <myjjue00@gmail.com>
2022-12-21 01:11:06 +0900

    Feat : Update Swagger version 2->3

--- src/main/java/com/example/likelionfinalproject/config/SwaggerConfig.java
+++ src/main/java/com/example/likelionfinalproject/config/SwaggerConfig.java
@@ -7,19 +7,17 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfig {
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.likelionfinalproject"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+        // http://localhost:8080/swagger-ui/
     }
-
-    // http://localhost:8080/swagger-ui.html
 }
