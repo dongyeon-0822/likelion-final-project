8fc990c0cb2c7f450051f83a8390a4f24cd264c1
DongYeon Kang <myjjue00@gmail.com>
2022-12-20 10:13:57 +0900

    Initial commit

---
+++ src/main/java/com/example/likelionfinalproject/LikelionFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelionfinalproject/LikelionFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
