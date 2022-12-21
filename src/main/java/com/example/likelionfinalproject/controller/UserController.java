2c14fa62a8406bb93bc429a39b5880d6355e84c0
DongYeon Kang <myjjue00@gmail.com>
2022-12-21 17:39:56 +0900

    Feat : Login Test 및 기본 기능 구현
    - Spring security test dependency 추가

--- src/main/java/com/example/likelionfinalproject/controller/UserController.java
+++ src/main/java/com/example/likelionfinalproject/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.likelionfinalproject.controller;
 
 import com.example.likelionfinalproject.domain.dto.UserJoinRequest;
+import com.example.likelionfinalproject.domain.dto.UserLoginRequest;
 import com.example.likelionfinalproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +21,11 @@ public class UserController {
     public ResponseEntity<String> join(@RequestBody UserJoinRequest dto) {
         userService.join(dto.getUserName(), dto.getPassword());
         return ResponseEntity.ok().body("회원가입 성공");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserLoginRequest dto) {
+        String token = userService.login(dto.getUserName(), dto.getPassword());
+        return ResponseEntity.ok().body(token);
     }
 }
--- src/main/java/com/example/likelionfinalproject/service/UserService.java
+++ src/main/java/com/example/likelionfinalproject/service/UserService.java
@@ -30,4 +30,18 @@ public class UserService {
 
         return "SUCCESS";
     }
+    public String login(String userName, String password) {
+        // userName 있는지 여부 확인, 없으면 NOT_FOUND 에러 발생
+        User selectedUser = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s는 가입된 적이 없습니다.", userName)));
+
+        // password 일치 하는지 여부 확인
+        if(!encoder.matches(password, selectedUser.getPassword())){
+            throw new AppException(ErrorCode.INVALID_PASSWORD, String.format("password를 잘못 입력했습니다. "));
+        }
+
+//        // 두가지 확인 중 예외 안났으면 Token 발행
+//        return JwtTokenUtil.createToken(userName, secretKey, expireTimeMs);
+        return "token";
+    }
 }
