439f3d7c5426d70bdf136cb2c30a9dc6f43e5d76
DongYeon Kang <myjjue00@gmail.com>
2022-12-21 11:10:30 +0900

    Feat : join 기본 기능 구현

---
+++ src/main/java/com/example/likelionfinalproject/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.likelionfinalproject.service;
+
+import com.example.likelionfinalproject.domain.entity.User;
+import com.example.likelionfinalproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public String join(String userName, String password) {
+        // userName 중복 체크
+        userRepository.findByUserName(userName)
+                .ifPresent(user -> {
+                    throw new RuntimeException(userName + "이미 존재합니다!");
+                });
+        User user = User.builder()
+                .userName(userName)
+                .password(password)
+                .build();
+        userRepository.save(user);
+
+        return "SUCCESS";
+    }
+}
