68b7260f47696059b2625e9c3eb0d8daa4294c20
DongYeon Kang <myjjue00@gmail.com>
2022-12-21 13:37:38 +0900

    Feat : Custom Exception 처리

--- src/main/java/com/example/likelionfinalproject/service/UserService.java
+++ src/main/java/com/example/likelionfinalproject/service/UserService.java
@@ -1,6 +1,8 @@
 package com.example.likelionfinalproject.service;
 
 import com.example.likelionfinalproject.domain.entity.User;
+import com.example.likelionfinalproject.exception.AppException;
+import com.example.likelionfinalproject.exception.ErrorCode;
 import com.example.likelionfinalproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,8 +17,9 @@ public class UserService {
         // userName 중복 체크
         userRepository.findByUserName(userName)
                 .ifPresent(user -> {
-                    throw new RuntimeException(userName + "이미 존재합니다!");
+                    throw new AppException(ErrorCode.USERNAME_DUPLICATED, userName + "이미 존재합니다!");
                 });
+
         User user = User.builder()
                 .userName(userName)
                 .password(password)
