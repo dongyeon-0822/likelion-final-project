46c3b9f895399ecdea818eb449281c95d18f188f
DongYeon Kang <myjjue00@gmail.com>
2022-12-26 03:07:22 +0900

    Feat : SQL exception 처리

--- src/main/java/com/example/likelionfinalproject/exception/ExceptionManager.java
+++ src/main/java/com/example/likelionfinalproject/exception/ExceptionManager.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
 @RestControllerAdvice
 public class ExceptionManager {
     @ExceptionHandler(AppException.class)
@@ -15,6 +17,14 @@ public class ExceptionManager {
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
                 .body(Response.error("ERROR", errorResponse));
     }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> sqlExceptionHandler(SQLException e){
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.DATABASE_ERROR, e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error("ERROR", errorResponse));
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
