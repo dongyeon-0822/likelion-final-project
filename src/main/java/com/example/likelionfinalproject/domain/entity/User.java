5bb463a0d68607a893592ea311f553b6753373d7
DongYeon Kang <myjjue00@gmail.com>
2022-12-27 17:57:24 +0900

    Test : 포스트 조회 성공, 등록 실패

--- src/main/java/com/example/likelionfinalproject/domain/entity/User.java
+++ src/main/java/com/example/likelionfinalproject/domain/entity/User.java
@@ -1,9 +1,6 @@
 package com.example.likelionfinalproject.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +10,7 @@ import javax.persistence.Id;
 @Entity
 @AllArgsConstructor
 @Getter
+@Setter
 @NoArgsConstructor
 @Builder
 public class User extends BaseTimeEntity{
