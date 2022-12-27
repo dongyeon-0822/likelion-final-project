3a5a39d88a8bc551165a78ae9f071ebd5ec50722
DongYeon Kang <myjjue00@gmail.com>
2022-12-27 14:49:57 +0900

    Test : 포스트 상세 조회 성공

--- src/test/java/com/example/likelionfinalproject/controller/PostControllerTest.java
+++ src/test/java/com/example/likelionfinalproject/controller/PostControllerTest.java
@@ -17,11 +17,13 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -61,7 +63,6 @@ class PostControllerTest {
             .andDo(print())
             .andExpect(status().isOk())
                 .andExpect(jsonPath("$.result.message").exists());
-
     }
 
     @Test
@@ -89,11 +90,32 @@ class PostControllerTest {
     }
 
     @Test
-    void getPostList() {
-    }
+    @DisplayName("포스트 상세 조회 성공")
+    @WithMockUser
+    void getPost() throws Exception {
+        PostDto postDto = PostDto.builder()
+                .id(1l)
+                .title("title")
+                .body("body")
+                .userName("user")
+                .createdAt(LocalDateTime.now())
+                .lastModifiedAt(LocalDateTime.now())
+                .build();
 
-    @Test
-    void getPost() {
+        when(postService.getPost(any()))
+                .thenReturn(postDto);
+
+        mockMvc.perform(get("/api/v1/posts/1")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.id").exists())
+                .andExpect(jsonPath("$.result.title").exists())
+                .andExpect(jsonPath("$.result.body").exists())
+                .andExpect(jsonPath("$.result.userName").exists())
+                .andExpect(jsonPath("$.result.createdAt").exists())
+                .andExpect(jsonPath("$.result.lastModifiedAt").exists());
+
     }
 
     @Test
