bc00d7cc8b058201131e57019de8e6a73f1ca704
DongYeon Kang <myjjue00@gmail.com>
2023-01-03 11:53:36 +0900

    Feat : post soft delete 추가

--- src/main/java/com/example/likelionfinalproject/domain/entity/Post.java
+++ src/main/java/com/example/likelionfinalproject/domain/entity/Post.java
@@ -1,6 +1,8 @@
 package com.example.likelionfinalproject.domain.entity;
 
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,6 +16,8 @@ import java.time.LocalDateTime;
 @Builder
 @Getter
 @Setter
+@SQLDelete(sql = "UPDATE post SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
 public class Post extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +28,8 @@ public class Post extends BaseTimeEntity{
     @ManyToOne
     @JoinColumn(name = "userId")
     private User user;
+
+    private boolean isDeleted = Boolean.FALSE;
 
     public static Post of(String title, String body, User user) {
         Post post = Post.builder()
