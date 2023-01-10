7ee9b255fb74507b69b5528efd7dbfc1c63ee9d7
DongYeon Kang <myjjue00@gmail.com>
2023-01-10 15:39:23 +0900

    Chore : Swagger UI api User, Alarm 명세 작성

--- src/main/java/com/example/likelionfinalproject/controller/AlarmController.java
+++ src/main/java/com/example/likelionfinalproject/controller/AlarmController.java
@@ -3,6 +3,7 @@ package com.example.likelionfinalproject.controller;
 import com.example.likelionfinalproject.domain.response.AlarmResponse;
 import com.example.likelionfinalproject.domain.response.Response;
 import com.example.likelionfinalproject.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +20,7 @@ public class AlarmController {
     private final UserService userService;
 
     @GetMapping("/alarms")
+    @ApiOperation(value = "알림 조회", notes = "로그인한 사용자는 특정 사용자가 게시한 게시물에 달린 댓글 및 좋아요에 대한 알림들을 한 페이지에 20개씩 조회 가능")
     public ResponseEntity<Response> getAlarms(Authentication authentication, Pageable pageable) {
         Page<AlarmResponse> alarms = userService.getAlarms(authentication.getName(), pageable);
         return ResponseEntity.ok().body(Response.success(alarms));
--- src/main/java/com/example/likelionfinalproject/controller/UserController.java
+++ src/main/java/com/example/likelionfinalproject/controller/UserController.java
@@ -9,6 +9,7 @@ import com.example.likelionfinalproject.domain.response.Response;
 import com.example.likelionfinalproject.domain.response.UserJoinResponse;
 import com.example.likelionfinalproject.domain.response.UserLoginResponse;
 import com.example.likelionfinalproject.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,12 +25,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
+    @ApiOperation(value = "회원가입", notes = "userName과 password로 회원가입, 단 userName 중복 불가능")
     public ResponseEntity<Response> join(@RequestBody UserJoinRequest dto) {
         UserJoinResponse userJoinResponse = userService.join(dto.getUserName(), dto.getPassword());
         return ResponseEntity.ok().body(Response.success(userJoinResponse));
     }
 
     @PostMapping("/login")
+    @ApiOperation(value = "로그인", notes = "userName과 password로 로그인 후 token 발급")
     public ResponseEntity<Response> login(@RequestBody UserLoginRequest dto) {
         UserLoginResponse userLoginResponse = userService.login(dto.getUserName(), dto.getPassword());
         return ResponseEntity.ok().body(Response.success(userLoginResponse));
